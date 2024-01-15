c201aca0b2950d5d88906683239819fd7a5a21d5
thisislaait <ogadi.ugo@gmail.com>
2024-01-15 19:46:41 +0100

    Fix: import inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class inventory(models.Model):
+class Inventory(models.Model):
     name = models.CharField(max_length =100, null=False, blank=False)
     cost_per_item = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
     quantity_in_stock = models.IntegerField(null=False, blank=False)
